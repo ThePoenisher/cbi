2c2b4cde877a91be039c77fd673ad030582ffdc0
Johannes <johannes@debussy>
2012-11-10 02:09:24 +0100

    verify-commit

--- bin/git-verify-commit-signature.sh
+++ bin/git-verify-commit-signature.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/../gpg
+export GNUPGHOME=$DIR/home 
+gpg2 --import $DIR/public-keys/* > /dev/null 2>&1
 key=""
 key=$(git log --format="format:" -n 1 --show-signature $1 | sed -nr 's/^\[GNUPG:\] VALIDSIG ([0-9A-F]{40}) .*/\1/p')
-test -n "$key" && grep -B 2 -A 1 $key $DIR/../gpg-keys/repo_authorization_keys
+test -n "$key" && grep -B 2 -A 1 $key $DIR/repo_authorization_keys
 
