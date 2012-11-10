6c058a77ecbd87778fbd0ca529203280a4229f28
Johannes <johannes@debussy>
2012-11-10 02:09:24 +0100

    test unsigned commit

--- bin/gpg-fpr.sh
+++ bin/gpg-fpr.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gpg2 -k --with-colons --with-fingerprint $1
-#| sed -ne 's/fpr:*\([^:]*\):$/\1/p'
+gpg2 -k $1 1>&2
+gpg2 -k --with-colons --with-fingerprint $1 | sed -ne 's/fpr:*\([0-9A-F]\{40\}\):$/\1/p'
