cceaf4254254a69210a86e59d07226fe449ce5dd
johannes@scriabin <johannes@scriabin>
2012-12-07 04:00:58 +0100

    a:wq

--- salt/etc/profile.d-cbi.sh
+++ salt/etc/profile.d-cbi.sh
@@ -1,6 +1,8 @@
 # -*- mode: shell-script; -*-
 CBI="{{ grains['cbi_home'] }}"
+CBI_MACHINE="{{ grains['cbi_machine'] }}"
 export CBI
+export CBI_MACHINE
 PATH=$PATH:$CBI/bin
 export PATH
 eval `keychain --eval -Q /home/johannes/.ssh/id_rsa`
